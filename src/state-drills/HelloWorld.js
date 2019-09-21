8ad10bf6b731751b7c6beb9abe066a765d83b6aa
Drew Carrillo <drewc3@gmail.com>
2019-09-21 10:55:12 -0600

    State drill

---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { who: "drew"}
+    // this.handleButtonClick= this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClickReact = () => {
+    this.setState({
+      who: 'React'
+    });
+  }
+  handleButtonClickFriend = () => {
+    this.setState({
+      who: 'Friend'
+    });
+  }
+  handleButtonClickWorld = () => {
+    this.setState({
+      who: 'World'
+    });
+  }
+
+  render() {
+    return(
+    <div>
+      <p>Hello, {this.state.who}</p>
+      <button onClick={this.handleButtonClickWorld}>World</button>
+      <button onClick={this.handleButtonClickFriend}>Friend</button>
+      <button onClick={this.handleButtonClickReact}>React</button>
+    </div>
+    )}
+}
+
+export default HelloWorld;
